7b87a3a8782e8764b3cce3f6a9022f453821c085
Nurul Islam <nurulbdpersonal@gmail.com>
2022-06-16 15:28:07 +0600

    final code

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+      <p>Generative Pre-trained Transformer 3 is an autoregressive language model that uses deep learning to produce human-like text. It is the third-generation language prediction model in the GPT-n series created by OpenAI, a San Francisco-based artificial intelligence research laboratory.</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
+
+export default Header;
